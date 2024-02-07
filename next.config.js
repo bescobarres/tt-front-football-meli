2f88fb8f6ccddb1c031bc9b2eaae79f82811187e
Bryan Escobar <bescobarres@gmail.com>
2024-02-07 17:41:37 +1030

    fix 6

--- next.config.js
+++ next.config.js
@@ -14,6 +14,21 @@ const nextConfig = {
         },
       ];
     },
+    async headers() {
+      return [
+        {
+          source: '/:path*{/}?',
+          headers: [
+            {
+              key: 'X-Accel-Buffering',
+              value: 'no',
+            },
+          ],
+        },
+      ]
+    },
 }
 
 module.exports = nextConfig
+
+
